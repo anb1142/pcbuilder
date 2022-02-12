1c9f37545a35cdb4cd1148898683b2598d117b4f
Anuja Nimesh <anujanimesh@gmail.com>
2022-02-12 20:35:23 +0530

    added titles for individual pages

--- layouts/InfoPage.jsx
+++ layouts/InfoPage.jsx
@@ -1,11 +1,15 @@
 import styles from "@styles/InfoPage.module.scss";
 import Layout from "@layouts/Layout";
+import Head from "next/head";
 
 export default function InfoPage({ className, h1style, title, children }) {
 	return (
 		<Layout className={styles.infopage}>
 			<h1 style={h1style ? h1style : {}}>{title}</h1>
 			<section className={className}>{children}</section>
+			<Head>
+				<title>PC Builder - {title}</title>
+			</Head>
 		</Layout>
 	);
 }
