a37b85d7f733b9469c56bfe7bfe570815a62c69a
Anuja Nimesh <anujanimesh@gmail.com>
2022-02-14 23:27:28 +0530

    bug fix

--- contexts/FaqContextProvider.tsx
+++ contexts/FaqContextProvider.tsx
@@ -1,6 +1,5 @@
 import { faqCollection } from "@data/firebase";
 import { FaqSpace } from "@stores/FaqStore";
-import { getDocs } from "firebase/firestore";
 import { createContext, useEffect, useState } from "react";
 import { useCollection } from "react-firebase-hooks/firestore";
 
@@ -8,22 +7,19 @@ export const FaqContext = createContext<FaqSpace.List[]>([]);
 
 const FaqContextProvider = (props: { children: any }) => {
 	const [faqs, setFaqs] = useState<FaqSpace.List[]>([]);
-
 	const [value] = useCollection(faqCollection);
 
-	const getData = async () => {
-		if (value) {
-			const data = await value.docs.map((doc) => ({
-				id: doc.id,
-				question: doc.data().question,
-				answer: doc.data().answer,
-			}));
-			console.log("data:", data);
-
-			setFaqs(data);
-		}
-	};
 	useEffect(() => {
+		const getData = async () => {
+			if (value) {
+				const data = await value.docs.map((doc) => ({
+					id: doc.id,
+					question: doc.data().question,
+					answer: doc.data().answer,
+				}));
+				setFaqs(data);
+			}
+		};
 		getData();
 	}, [value]);
 
