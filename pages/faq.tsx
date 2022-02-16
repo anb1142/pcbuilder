b5b007a3e87f8f28411fa47bacb7cfad3c048718
Anuja Nimesh <anujanimesh@gmail.com>
2022-02-16 18:36:19 +0530

    admin bug fix

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -2,17 +2,19 @@ import FaqAdmin from "@components/FaqAdmin";
 import FaqList from "@components/FaqList";
 import Loading from "@components/Loading";
 import { FaqContext } from "@contexts/FaqContextProvider";
-import { FirebaseContext } from "@contexts/FirebaseContextProvider";
+import { UserContext } from "@contexts/UserContextProvider";
 import InfoPage from "@layouts/InfoPage";
 import { useContext, useState } from "react";
 
 const Faq = () => {
-	const user = useContext(FirebaseContext);
+	const user = useContext(UserContext);
 
 	const [removeId, setRemoveId] = useState<string>("");
 	const [confirmFaqIsOpen, setConfirmFaqOpen] = useState<boolean>(false);
 	const [faqIsOpen, setFaqOpen] = useState<boolean>(false);
 	const faqs = useContext(FaqContext);
+	console.log(user);
+
 	return (
 		<InfoPage h1style={user ? { marginBottom: 0 } : {}} title="Questions and Answers">
 			{faqs.length === 0 ? (
